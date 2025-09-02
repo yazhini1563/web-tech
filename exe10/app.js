9f5a10eecb0c36542a9e7b055508e6097b70274c
yazhini1563 <yazhinikavitha0@gmail.com>
2025-09-02 11:06:07 +0530

    5

---
+++ exe10/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World from Home Page\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
